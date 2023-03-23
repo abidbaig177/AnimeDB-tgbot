458b3386919e9772b5a3bf1f50c91a02aa122d32
abidbaig177 <119119875+abidbaig177@users.noreply.github.com>
2023-03-23 22:02:24 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,5 +4,5 @@ import { cleanEnv, str } from "envalid";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  BOT_TOKEN: str(),
+  BOT_TOKEN: str(), "6013387746:AAERqoJjTWCpwrk2TY-szM4itBS-ajOqewU")
 });
